9e623dd4c1131288a63523abe80b2863dbe3871c
icaro156 <icaro1561@outlook.com>
2020-05-18 19:19:24 -0300

    modulo 2 de base

--- modulo02/src/app/controllers/sessionController.js
+++ modulo02/src/app/controllers/sessionController.js
@@ -1,10 +1,19 @@
 import jwt from 'jsonwebtoken';
+import * as Yup from 'yup';
 
 import authConfig from '../../config/auth';
 import User from '../models/user';
 
 class SessionController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      email: Yup.string().email().required(),
+      password: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      res.status(400).json({ error: 'invalid request' });
+    }
     const { email, password } = req.body;
 
     const user = await User.findOne({ where: { email } });
