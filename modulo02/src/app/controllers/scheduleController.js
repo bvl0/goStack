f210e882b151f42924ac6feecb02ce65ed44434b
icaro156 <icaro1561@outlook.com>
2020-06-04 22:27:09 -0300

    modulo 3.2 completo

---
+++ modulo02/src/app/controllers/scheduleController.js
@@ -0,0 +1,22 @@
+import { endOfDay, startOfDay, parseISO } from 'date-fns';
+import { Op } from 'sequelize';
+import Appointment from '../models/appointment';
+0
+class ScheduleController {
+  async index(req, res) {
+    const { date } = req.query;
+    const schedule = await Appointment.findAll({
+      where: {
+        provider_id: req.userId,
+        canceled_at: null,
+        date: {
+          [Op.between]: [startOfDay(parseISO(date)), endOfDay(parseISO(date))],
+        },
+      },
+    });
+
+    return res.json(schedule);
+  }
+}
+
+export default new ScheduleController();
--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -1,11 +1,18 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerconfig from './config/multer';
 
 import UserController from './app/controllers/userController';
 import SessionController from './app/controllers/sessionController';
+import FileController from './app/controllers/fileController';
+import ProviderController from './app/controllers/providerController';
+import AppointmentController from './app/controllers/appointmentController'
+import ScheduleController from './app/controllers/scheduleController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerconfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
@@ -14,4 +21,12 @@ routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 
+routes.get('/providers', ProviderController.index);
+
+routes.post('/appointments', AppointmentController.store);
+routes.get('/appointments', AppointmentController.index);
+routes.get('/schedules', ScheduleController.index);
+
+
+routes.post('/files', upload.single('file'), FileController.store);
 export default routes;
