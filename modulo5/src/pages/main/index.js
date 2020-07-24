16aca4134fac32fd0f6b270cc016a32accc1895f
icaro156 <icaro1561@outlook.com>
2020-07-24 14:46:58 -0300

    Estilização em andamento

--- modulo5/src/pages/main/index.js
+++ modulo5/src/pages/main/index.js
@@ -4,8 +4,8 @@ import {Link} from 'react-router-dom'
 
 import api from '../../Services/api';
 
-
-import { Container, Form, SubmitButton, List } from './styles';
+import Container from '../../components/container'
+import { Form, SubmitButton, List } from './styles';
 
   class Main extends Component {
     state ={
--- modulo5/src/pages/repository/index.js
+++ modulo5/src/pages/repository/index.js
@@ -1,22 +1,63 @@
 import React, { Component } from 'react';
+import Proptypes from 'prop-types';
 import api from '../../Services/api';
+import { Link } from 'react-router-dom'
 
-// import { Container } from './styles';
+import { Loading, Owner } from './styles';
+import Container from '../../components/container';
 
 export default class Repository extends Component {
+  static propTypes = {
+    match: Proptypes.shape({
+      params: Proptypes.shape({
+        repository: Proptypes.string,
+      })
+    }).isRequired,
+  }
+
+  state = {
+    repositorys: {},
+    issues: [],
+    loading: true,
+  };
+
   async componentDidMount() {
     const { match } = this.props;
 
     const repoName = decodeURIComponent(match.params.repository);
 
-    const reponse = await api.get(`/repos/${repoName}`);
-    const issues = await api.get(`/repos/${repoName}/issues`);
 
-    const [repo, issues] = Promises
+    const [ repo, issues ] = await Promise.all([
+       api.get(`/repos/${repoName}`),
+       api.get(`/repos/${repoName}/issues`, {
+         params:{
+           state: 'open',
+           per_page: 5,
+         },
+       }),
+    ]);
+
+    this.setState({
+      repository: repo.data,
+      issues: issues.data,
+      loading: false,
+    })
   }
 
   render() {
+    const {repository, issues, loading} = this.state;
+    if(loading) {
+      return <Loading>Carregando</Loading>
+    }
 
+    return <Container>
+      <Owner>
+        <Link to= "/">Voltar aos repositórios</Link>
+        <img src={repository.owner.avatar_url} alt={repository.owner.login}/>
+        <h1>{repository.name}</h1>
+        <p>{repository.description}</p>
+      </Owner>
+    </Container>;
   }
 }
 
